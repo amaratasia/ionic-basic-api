fbdf13a3888cdca8b1c909fdf95b36a2f500b20e
Amar <amaratasia@gmail.com>
2018-11-18 15:16:01 +0530

    Changes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ApplicationRecord
 	has_many :recipe_ingredients
 
 	def complete_cooking(user_id)
-		recipe_ingre = recipe.recipe_ingredients.map{|x| { x.ingredient_id => x.qty}}.inject(:merge)
+		recipe_ingre = recipe_ingredients.map{|x| { x.ingredient_id => x.qty}}.inject(:merge)
 		user_ingre = User.find(user_id).user_ingredients.where(ingredient_id: recipe_ingre.keys)
 		user_ingre.each do |ingre|
 			ingre.quanity = ingre.quanity.to_i - recipe_ingre[ingre.ingredient_id].to_i
